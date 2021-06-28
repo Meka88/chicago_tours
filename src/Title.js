d269bc2cb741ad1e07552538e3e58cc9e1f21d92
meka <mekabolzhirova@gmail.com>
2021-06-28 16:26:23 -0400

    header component built

---
+++ src/Title.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Title.css';
+
+function Title() {
+    return (
+        <div>
+            <h1>CHICAGOTOUR</h1>
+            <div className="Title-Subtitle">Bets places to see in Chicago</div>
+        </div>
+    )
+}
+
+export default Title
